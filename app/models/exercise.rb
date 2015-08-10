5b40165ac7767cffb11e07000d50b90cdcdfb1ca
Mashrur Hossain <railsudemy@gmail.com>
2015-08-10 12:27:25 +0000

    Add validations to exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,10 @@
 class Exercise < ActiveRecord::Base
   belongs_to :user
+  
+  validates :duration_in_min, presence: true
+  validates :duration_in_min, numericality: { only_integer: true }
+  validates :workout, presence: true
+  validates :workout_date, presence: true
+  validates :user_id, presence: true
+  
 end
