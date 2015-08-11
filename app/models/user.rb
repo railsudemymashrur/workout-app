277079c127bc4f48f9a8d1d731f9ff182b11c66b
Mashrur Hossain <railsudemy@gmail.com>
2015-08-11 18:41:07 +0000

    Add validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :exercises
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
   def full_name
     "#{first_name} #{last_name}"
--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -16,4 +16,18 @@ RSpec.feature "Users signup" do
     visit "/"
     expect(page).to have_content("John Doe")
   end
+  scenario "with invalid credentials" do
+    visit "/"
+    
+    click_link "Sign up"
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
+  end
 end
